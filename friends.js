2011bdda827576f81ba2b4153ab7d5ea3fb112d8
unk-pn <zver2006.11@gmail.com>
2025-03-14 22:54:56 +0300

    1

---
+++ friends.js
@@ -0,0 +1,29 @@
+//Массив images содержит пути к изображениям.
+const images = [
+    "./src/images/dog.png",
+    "./src/images/cattt.svg",
+    "./src/images/big_dog.png",
+  ];
+  //Переменная currentIndex хранит индекс текущего изображения.
+  let currentIndex = 0;
+  
+  //Функция showSlide(index) обновляет изображение в слайдере.
+  function showSlide(index) {
+    const slide = document.getElementById("slide");
+    slide.src = images[index];
+  }
+  
+  //Функции nextSlide() и prevSlide() переключают изображения вперёд и назад соответственно, , используя новый currentIndex.
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    //Оператор % (остаток от деления) используется для обеспечения циклического перехода. 
+    //Когда currentIndex достигает конца массива, он сбрасывается на 0, чтобы начать с первого изображения снова.
+    showSlide(currentIndex);
+  }
+  
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showSlide(currentIndex);
+  }
+  //Использование оператора % гарантирует, что слайдер будет переключаться по кругу, даже если достигнут конец массива.
+  
